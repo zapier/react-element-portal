6d7a437212c9850424bf4ed24ba7d62a82d50155
ngryman <ngryman@gmail.com>
2017-10-20 11:41:53 +0200

    correctly check for ReactDOM.createPotal

--- src/ElementPortal.js
+++ src/ElementPortal.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
 import FiberElementPortal from './FiberElementPortal';
 import LegacyElementPortal from './LegacyElementPortal';
 
-const hasNativePortal = 'createPortal' in React;
+const hasNativePortal = 'createPortal' in ReactDOM;
 export default hasNativePortal ? FiberElementPortal : LegacyElementPortal;
